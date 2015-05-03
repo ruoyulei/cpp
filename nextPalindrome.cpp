e6fc9dfbdb05e112b8b922056296e0e8fcd6886d
Ruoyu Lei <ruoyu@Ruoyus-MacBook-Pro.local>
2015-05-02 22:51:40 -0400

    nextPalindrome

--- nextPalindrome.cpp
+++ nextPalindrome.cpp
@@ -7,43 +7,43 @@ bool isPalindrome(long n);
 long findNext(long n);
 
 bool isPalindrome(long n) {
-		  /* flip the parameter to see*/
-		  if(n <10) {
-					 return true;
-		  }
+    /* flip the parameter to see*/
+    if(n <10) {
+        return true;
+    }
 
-		  std::string origin = std::to_string(n);
-		  std::string flip="";
+    std::string origin = std::to_string(n);
+    std::string flip="";
 
-		  for(long i = origin.length(); i >0;i--) {
-					 flip += origin[i-1];
-		  }
+    for(long i = origin.length(); i >0;i--) {
+        flip += origin[i-1];
+    }
 
-		  return (origin.compare(flip) == 0);
+    return (origin.compare(flip) == 0);
 }
 
 long findNext(long n) {
-		  if(isPalindrome(n))
-					 return n;
-		  else
-					 return findNext(n+1);
+    if(isPalindrome(n))
+        return n;
+    else
+        return findNext(n+1);
 }
 
 int main() {
-		  long index;
-		  cin >> index;
+    long index;
+    cin >> index;
 
-		  long arr[index];
-		  long temp;
+    long arr[index];
+    long temp;
 
-		  for(long i =0; i < index; i++) {
-					 cin >> temp;
-					 arr[i] = temp;
-		  }
+    for(long i =0; i < index; i++) {
+        cin >> temp;
+        arr[i] = temp;
+    }
 
-		  for(long i = 0; i < index; i++) {
-					 cout << findNext(arr[i]+1) << "\n";
-		  }
+    for(long i = 0; i < index; i++) {
+        cout << findNext(arr[i]+1) << "\n";
+    }
 
-		  return 0;
+    return 0;
 }
