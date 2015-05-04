83f086d0591cc5ff570a0543d8c9133a3d6e08ec
Ruoyu Lei <ruoyu@Ruoyus-MacBook-Pro.local>
2015-05-03 21:42:35 -0400

    gasStation

---
+++ gasStation.cpp
@@ -0,0 +1,12 @@
+/*  
+ *  http://www.lintcode.com/zh-cn/problem/gas-station/
+ */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int canCompleteCircuit(vector<int> &gas, vector<int> &cost);
+
